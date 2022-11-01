c24780a1fe013d9197a6d0345516cd067dabe2af
Juan Cazala <juancazala@gmail.com>
2022-11-01 18:50:24 -0300

    chore: added comments

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,6 +1,10 @@
 import * as vscode from 'vscode'
 const output = vscode.window.createOutputChannel(`Decentraland`)
 
+/**
+ * Util to print messages to the Output channel
+ * @param message a string to print
+ */
 export function log(message: string) {
   output.appendLine(message)
 }
--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,2 +1,7 @@
+/**
+ * Returns a promise that resolves in a given amount of milliseconds
+ * @param ms amount of milliseconds
+ * @returns
+ */
 export const sleep = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms))
