6004c43e053e2773f31271a734005e60155a04fc
Juan Cazala <juancazala@gmail.com>
2022-11-21 15:33:17 -0300

    feat: setup debugger

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,13 @@ import {
   startServer as startDCLPreview,
   stopServer as stopDCLPreview,
 } from './dcl-preview/server'
-import { getCwd, isDCL, isEmpty, setExtensionPath, setGlobalStoragePath } from './utils/path'
+import {
+  getCwd,
+  isDCL,
+  isEmpty,
+  setExtensionPath,
+  setGlobalStoragePath,
+} from './utils/path'
 import { install } from './commands/install'
 import { start } from './commands/start'
 import { browser } from './commands/browser'
@@ -19,7 +25,7 @@ import { init } from './commands/init'
 import { restart } from './commands/restart'
 import { Dependency } from './dependencies/types'
 import { npmInstall, npmUninstall } from './utils/npm'
-import { ServerName } from './utils/port'
+import { getServerParams, getServerUrl, ServerName } from './utils/port'
 import { ProjectType } from './utils/project'
 import { checkBinaries, resolveVersion, setVersion } from './utils/node'
 import { unwatch, watch } from './utils/watch'
@@ -45,10 +51,21 @@ export async function activate(context: vscode.ExtensionContext) {
 
   // Helper ro register a command
   const disposables: vscode.Disposable[] = []
-  const register = (command: string, callback: (...args: any[]) => any) => disposables.push(vscode.commands.registerCommand(command, callback))
+  const register = (command: string, callback: (...args: any[]) => any) =>
+    disposables.push(vscode.commands.registerCommand(command, callback))
 
   // Register GLTF preview custom editor
   GLTFPreviewEditorProvider.register(context, disposables)
+
+  vscode.debug.registerDebugConfigurationProvider(
+    'decentraland',
+    {
+      resolveDebugConfiguration() {
+        return null
+      },
+    },
+    vscode.DebugConfigurationProviderTriggerKind.Dynamic
+  )
 
   // Decentraland Commands
   register('decentraland.commands.init', () => init().then(validate))
@@ -56,28 +73,38 @@ export async function activate(context: vscode.ExtensionContext) {
   register('decentraland.commands.install', () => install())
   register('decentraland.commands.uninstall', () => uninstall())
   register('decentraland.commands.start', () => start())
+  register('decentraland.commands.getDebugURL', async () => `${await getServerUrl(ServerName.DCLPreview)}${await getServerParams(ServerName.DCLPreview)}`)
   register('decentraland.commands.restart', () => restart())
   register('decentraland.commands.deploy', () => deploy())
-  register('decentraland.commands.deployCustom',
-    async () =>
-      deploy(
-        `--target ${await vscode.window.showInputBox({
-          title: 'Deploy to custom Catalyst',
-          prompt: 'Enter the URL of the Catalyst',
-          placeHolder: 'peer-testing.decentraland.org',
-        })}`
-      )
+  register('decentraland.commands.deployCustom', async () =>
+    deploy(
+      `--target ${await vscode.window.showInputBox({
+        title: 'Deploy to custom Catalyst',
+        prompt: 'Enter the URL of the Catalyst',
+        placeHolder: 'peer-testing.decentraland.org',
+      })}`
+    )
   )
-  register('decentraland.commands.browser.run', () => browser(ServerName.DCLPreview))
-  register('decentraland.commands.browser.deploy', () => browser(ServerName.DCLDeploy))
+  register('decentraland.commands.browser.run', () =>
+    browser(ServerName.DCLPreview)
+  )
+  register('decentraland.commands.browser.deploy', () =>
+    browser(ServerName.DCLDeploy)
+  )
 
   // Dependencies
   registerTree(disposables)
-  register('dependencies.commands.delete', (node: Dependency) => npmUninstall(node.label))
-  register('dependencies.commands.update', (node: Dependency) => npmInstall(`${node.label}@latest`))
+  register('dependencies.commands.delete', (node: Dependency) =>
+    npmUninstall(node.label)
+  )
+  register('dependencies.commands.update', (node: Dependency) =>
+    npmInstall(`${node.label}@latest`)
+  )
 
   // Walkthrough
-  register('walkthrough.createProject', () => init(ProjectType.SCENE).then(validate))
+  register('walkthrough.createProject', () =>
+    init(ProjectType.SCENE).then(validate)
+  )
   register('walkthrough.viewCode', () => {
     vscode.commands.executeCommand(
       'vscode.openFolder',
