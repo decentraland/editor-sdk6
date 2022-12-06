25949853e645af787810253962a768de02821a9f
Juan Cazala <juancazala@gmail.com>
2022-12-06 15:11:27 -0300

    chore: added more test for node module

--- src/utils/node.spec.ts
+++ src/utils/node.spec.ts
@@ -1,11 +1,23 @@
 import fs from 'fs'
 import { checkBinaries, getVersion, resolveVersion, setVersion } from './node'
-import {
-  getGlobalBinPath,
-  setGlobalStoragePath,
-  getNodeBinPath,
-  getNodeCmdPath,
-} from './path'
+import { getGlobalBinPath, setGlobalStoragePath } from './path'
+
+import { Readable } from 'stream'
+
+class DummyBody extends Readable {
+  done = false
+  _read(_size: number): void {
+    if (!this.done) {
+      this.push('test')
+      this.done = true
+    } else {
+      this.push(null)
+    }
+  }
+  _destroy(): void {
+    this.done = true
+  }
+}
 
 import { getPackageJson } from './pkg'
 jest.mock('./pkg')
@@ -17,13 +29,21 @@ import { log } from './log'
 jest.mock('./log')
 const logMock = log as jest.MockedFunction<typeof log>
 
+import { sleep } from './sleep'
+jest.mock('./sleep')
+const sleepMock = sleep as jest.MockedFunction<typeof sleep>
+
 import { track } from './analytics'
 jest.mock('./analytics')
 const trackMock = track as jest.MockedFunction<typeof track>
 
-import fetch from 'node-fetch'
+import fetch, { Headers, Response } from 'node-fetch'
 jest.mock('node-fetch')
 const fetchMock = fetch as jest.MockedFunction<typeof fetch>
+
+import rimraf from 'rimraf'
+jest.mock('rimraf')
+const rimrafMock = rimraf as jest.MockedFunction<typeof rimraf>
 
 jest.spyOn(fs, 'readdir')
 const fsReaddirMock = fs.readdir as unknown as jest.MockedFunction<
@@ -41,6 +61,8 @@ jest.spyOn(fs, 'symlinkSync')
 const fsSymlinkSyncMock = fs.symlinkSync as jest.MockedFunction<
   typeof fs.symlinkSync
 >
+jest.spyOn(fs, 'mkdirSync')
+const fsMkdirSyncMock = fs.mkdirSync as jest.MockedFunction<typeof fs.mkdirSync>
 
 describe('node', () => {
   beforeAll(() => {
@@ -206,13 +228,17 @@ describe('node', () => {
       describe('and there are distributions installed', () => {
         beforeEach(() => {
           fsReaddirMock.mockImplementationOnce((_path, callback) => {
-            callback(null, ['node-v16.1.0-win-x64', 'node-v16.0.8-win-x64'])
+            callback(null, [
+              'node-v16.1.0-win-x64',
+              'node-v16.0.8-win-x64',
+              'node-v17.0.0-win-x64',
+            ])
           })
         })
         afterEach(() => {
           fsReaddirMock.mockReset()
         })
-        it('should log the latest version installed', async () => {
+        it('should log the latest version installed from the same major', async () => {
           await resolveVersion()
           expect(logMock).toHaveBeenCalledWith(
             `Latest node version installed: 16.1.0`
@@ -326,20 +352,10 @@ describe('node', () => {
       })
       describe('and distribution is already installed and linked', () => {
         beforeEach(() => {
-          fsExistsSyncMock.mockImplementation((path) => {
-            const nodeBinPath = getNodeBinPath()
-            const nodeCmdPath = getNodeCmdPath()
-            switch (path) {
-              case nodeBinPath: {
-                return true
-              }
-              case nodeCmdPath: {
-                return true
-              }
-              default:
-                return false
-            }
-          })
+          // check if distribution is installed
+          fsExistsSyncMock.mockReturnValueOnce(true)
+          // check if distribution is linked
+          fsExistsSyncMock.mockReturnValueOnce(true)
         })
         afterEach(() => {
           fsExistsSyncMock.mockReset()
@@ -367,17 +383,11 @@ describe('node', () => {
       })
       describe('and distribution is already installed but not linked', () => {
         beforeEach(() => {
-          fsExistsSyncMock.mockImplementation((path) => {
-            const nodeBinPath = getNodeBinPath()
-            switch (path) {
-              case nodeBinPath: {
-                return true
-              }
-              default:
-                return false
-            }
-          })
-          fsSymlinkSyncMock.mockReturnValue()
+          // check if distribution is installed
+          fsExistsSyncMock.mockReturnValueOnce(true)
+          // check if distribution is linked
+          fsExistsSyncMock.mockReturnValueOnce(false)
+          fsSymlinkSyncMock.mockReturnValueOnce()
         })
         afterEach(() => {
           fsExistsSyncMock.mockReset()
@@ -390,18 +400,70 @@ describe('node', () => {
             '/globalStorage/node'
           )
         })
-        it('should log the linked binaries', async () => {
+      })
+      describe('and distribution is neither installed nor linked', () => {
+        beforeEach(() => {
+          setVersion('16.2.0')
+          // check if distribution is installed
+          fsExistsSyncMock.mockReturnValue(false)
+          // check if has bin dir
+          fsExistsSyncMock.mockReturnValue(false)
+          // check if distribution is linked
+          fsExistsSyncMock.mockReturnValueOnce(false)
+          fsSymlinkSyncMock.mockReturnValueOnce()
+          fsMkdirSyncMock.mockReturnValueOnce('')
+          sleepMock.mockResolvedValueOnce(0)
+          // mock and older distribution installed so it uninstalls it
+          fsReaddirMock.mockImplementationOnce((_path, callback) => {
+            callback(null, ['node-v16.1.0-win-x64'])
+          })
+          fetchMock.mockResolvedValueOnce({
+            ok: true,
+            body: new DummyBody(),
+            headers: {
+              get: jest.fn().mockReturnValueOnce(4), // "content-length" of 4 bytes (string "test")
+            },
+          } as any)
+          // mock removal of old installed distribution
+          rimrafMock.mockImplementationOnce((_path, cb) => cb(null))
+          // mock removal of old linked distribution
+          rimrafMock.mockImplementationOnce((_path, cb) => cb(null))
+        })
+        afterEach(() => {
+          setVersion(null)
+          fsExistsSyncMock.mockReset()
+          fsSymlinkSyncMock.mockReset()
+          fsMkdirSyncMock.mockReset()
+          fsReaddirMock.mockReset()
+          fetchMock.mockReset()
+          rimrafMock.mockReset()
+          sleepMock.mockReset()
+        })
+        it('should create the bin directory', async () => {
           await checkBinaries()
-          expect(logMock).toHaveBeenCalledWith(
-            'Linking distribution using symlink (unix)...'
+          expect(fsMkdirSyncMock).toHaveBeenCalledWith('/globalStorage/bin', {
+            recursive: true,
+          })
+        })
+        it('should uninstall the old distribution', async () => {
+          await checkBinaries()
+          expect(rimrafMock).toHaveBeenCalledWith(
+            '/globalStorage/bin/node-v16.1.0-win-x64',
+            expect.any(Function)
           )
-          expect(logMock).toHaveBeenCalledWith(
-            'Link from:',
+        })
+        it('should unlink the old distribution', async () => {
+          await checkBinaries()
+          expect(rimrafMock).toHaveBeenCalledWith(
+            '/globalStorage/node',
+            expect.any(Function)
+          )
+        })
+        it('should link the node binaries', async () => {
+          await checkBinaries()
+          expect(fsSymlinkSyncMock).toHaveBeenCalledWith(
+            '/globalStorage/bin/node-v16.2.0-darwin-arm64/bin/node',
             '/globalStorage/node'
-          )
-          expect(logMock).toHaveBeenCalledWith(
-            'Link to:',
-            '/globalStorage/bin/node-v0.0.0-test-darwin-arm64/bin/node'
           )
         })
       })
