d509e598866ccaa80c9ad4bf91a92231a6fab151
Juan Cazala <juancazala@gmail.com>
2022-12-15 13:20:51 -0300

    chore: added activation analytics

--- src/modules/pkg.spec.ts
+++ src/modules/pkg.spec.ts
@@ -1,15 +1,17 @@
+import { getPackageJson, getPackageVersion } from './pkg'
+
 /********************************************************
                           Mocks
 *********************************************************/
 
-import { getExtensionPath } from './path'
+import { getExtensionPath, getCwd } from './path'
 jest.mock('./path')
 const getExtensionPathMock = getExtensionPath as jest.MockedFunction<
   typeof getExtensionPath
 >
+const getCwdMock = getCwd as jest.MockedFunction<typeof getCwd>
 
 import fs from 'fs'
-import { getPackageJson } from './pkg'
 jest.mock('fs')
 const fsReadFileSyncMock = fs.readFileSync as jest.MockedFunction<
   typeof fs.readFileSync
@@ -21,12 +23,14 @@ const fsReadFileSyncMock = fs.readFileSync as jest.MockedFunction<
 
 describe('pkg', () => {
   beforeEach(() => {
-    fsReadFileSyncMock.mockReturnValue('{ "name": "A module" }')
+    fsReadFileSyncMock.mockReturnValue('{ "version": "0.0.0-test" }')
     getExtensionPathMock.mockReturnValue('/path/to/extension')
+    getCwdMock.mockReturnValue('/path/to/workspace')
   })
   afterEach(() => {
     fsReadFileSyncMock.mockReset()
     getExtensionPathMock.mockReset()
+    getCwdMock.mockReset()
   })
   describe('When getting the package.json of the extension', () => {
     it('should read the package.json of the extension', () => {
@@ -37,7 +41,21 @@ describe('pkg', () => {
       )
     })
     it('should return the parsed package.json', () => {
-      expect(getPackageJson()).toEqual({ name: 'A module' })
+      expect(getPackageJson()).toEqual({ version: '0.0.0-test' })
+    })
+  })
+  describe('When getting the package.json of the workspace', () => {
+    it('should read the package.json from the workspace', () => {
+      getPackageJson(null, true)
+      expect(fsReadFileSyncMock).toHaveBeenCalledWith(
+        '/path/to/workspace/package.json',
+        'utf8'
+      )
+    })
+  })
+  describe('When getting the version of the package.json of the extension', () => {
+    it('should return the version of the package.json', () => {
+      expect(getPackageVersion()).toEqual('0.0.0-test')
     })
   })
   describe('When getting the package.json of a module', () => {
@@ -61,6 +79,37 @@ describe('pkg', () => {
         expect(() => getPackageJson('some-non-existent-module')).toThrow(
           `Could not get package.json for module "some-non-existent-module": Not found`
         )
+      })
+    })
+  })
+  describe('When getting the package.json of a module in the workspace', () => {
+    it('should read the package.json from that module in the workspace', () => {
+      getPackageJson('some-module', true)
+      expect(fsReadFileSyncMock).toHaveBeenCalledWith(
+        '/path/to/workspace/node_modules/some-module/package.json',
+        'utf8'
+      )
+    })
+  })
+  describe('When getting the version of the package.json of a module', () => {
+    it('should read the package.json from that module', () => {
+      getPackageVersion('some-module')
+      expect(fsReadFileSyncMock).toHaveBeenCalledWith(
+        '/path/to/extension/node_modules/some-module/package.json',
+        'utf8'
+      )
+    })
+    describe('and the module does not exist', () => {
+      beforeEach(() => {
+        fsReadFileSyncMock.mockImplementation(() => {
+          throw new Error('Not found')
+        })
+      })
+      afterEach(() => {
+        fsReadFileSyncMock.mockReset()
+      })
+      it('should return null', () => {
+        expect(getPackageVersion('some-non-existent-module')).toBeNull()
       })
     })
   })
--- src/modules/pkg.ts
+++ src/modules/pkg.ts
@@ -1,33 +1,44 @@
 import fs from 'fs'
 import path from 'path'
-import { getExtensionPath } from './path'
+import { getCwd, getExtensionPath } from './path'
 
 /**
  * Return the package json of a given module
  * @param moduleName The name of the module
  * @returns The package json object
  */
-export function getPackageJson(moduleName?: string): {
+export function getPackageJson(
+  moduleName?: string | null,
+  workspace = false
+): {
   version: string
   engines: {
     node: string
   }
   bin?: { [command: string]: string }
 } {
-  const packageJsonPath =
-    typeof moduleName === 'string'
-      ? path.join(
-          getExtensionPath(),
-          './node_modules',
-          moduleName,
-          'package.json'
-        )
-      : path.join(getExtensionPath(), 'package.json')
+  const basePath = workspace ? getCwd() : getExtensionPath()
+  const packageJsonPath = !!moduleName
+    ? path.join(basePath, './node_modules', moduleName, 'package.json')
+    : path.join(basePath, 'package.json')
   try {
     return JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
   } catch (error: any) {
     throw new Error(
       `Could not get package.json for module "${moduleName}": ${error.message}`
     )
+  }
+}
+
+/**
+ * Returns the version of a given module if exists, otherwise returns null
+ */
+
+export function getPackageVersion(moduleName?: string, workspace = false) {
+  try {
+    const pkg = getPackageJson(moduleName, workspace)
+    return pkg.version
+  } catch (error) {
+    return null
   }
 }
