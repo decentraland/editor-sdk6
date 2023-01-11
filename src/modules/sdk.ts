1a456726e96cd9416345a3d5a71e89b05a06711c
Juan Cazala <juancazala@gmail.com>
2023-01-11 14:00:27 -0300

    feat: upgrade cli to v3.15.0 (#68)
    * fix: only sync sdk if it is installed
    
    * chore: upgrade dependencies

--- src/modules/sdk.ts
+++ src/modules/sdk.ts
@@ -4,14 +4,18 @@ import { getPackageVersion } from './pkg'
 
 export async function syncSdkVersion() {
   const extensionSdkVersion = getPackageVersion('@dcl/sdk')
-  log(`Extension SDK version: ${extensionSdkVersion}`)
   const workspaceSdkVersion = getPackageVersion('@dcl/sdk', true)
-  log(`Workspace SDK version: ${workspaceSdkVersion}`)
+  if (!workspaceSdkVersion) {
+    // no need to sync if the workspace does not have a dependency on @dcl/sdk
+    return
+  }
   if (extensionSdkVersion !== workspaceSdkVersion) {
+    log(`Extension @dcl/sdk version: ${extensionSdkVersion}`)
+    log(`Workspace @dcl/sdk version: ${workspaceSdkVersion}`)
     log(
-      `Workspace SDK version is different than the extension\'s, installing @dcl/sdk@${extensionSdkVersion} into workspace...`
+      `Workspace @dcl/sdk version is different than the extension\'s, installing @dcl/sdk@${extensionSdkVersion} into workspace...`
     )
     await npmInstall(`@dcl/sdk@${extensionSdkVersion}`)
   }
-  log(`Workspace SDK version is up to date`)
+  log(`Workspace @dcl/sdk version is up to date`)
 }
